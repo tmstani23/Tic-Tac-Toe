62e6528d12f3e40cb8c7e1d8e841845a1ea0b843
Tim Work Laptop <tmstani23@gmail.com>
2018-01-29 16:29:30 -0600

    Update 2
    Updated winning board function so it correctly checks board for all possible win combinations and assigns input player a score of 10.  Added skeleton comp move function that creates a temp version of the board and assesses win/loss states then chooses a move.  Not functional.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("helloworld");
+
 //create game board
 //X's turn
 //X O O
@@ -7,82 +7,124 @@ console.log("helloworld");
 //create a tic tac toe game where the computer never loses:
 let compP = "X";
 let player = "O";
-let currentBoard = ["X", "O", "O", 
-                    "X",   4,    5,    
+let currentBoard = ["O", 1, 2, 
+                    "X", "X", 5,    
                     "X", "O", "X"];
+let tempBoard = [];
 let score;
 //define rules of the game by indices:
-//3 in a row horiz
-    //col 1
+//create winning board function:
+
         //if index 0 and 3 and 6 = compP:
-        if(currentBoard[0] == compP && currentBoard[3] == compP && currentBoard[6] == compP) {
-            //comp score = +10
-            score = 10;
-            //return score;
-        }
-            
-        //if index 0 and 3 and 6 = player:
-        if(currentBoard[0] == player && currentBoard[3] == player && currentBoard[6] == player) {
-            //player score = -10
-            score = -10;
-            //return score;
-        }    
-        
+
+// function displayMsg(msg) {
+//     document.getElementById("outputP").innerHTML = msg;
+// }        
+function winningBoard(player, board) {   
+        //3 in a row vertical
+        //col 1
+    if(board[0] == player && board[3] == player && board[6] == player) {
+        score = 10;
+        return score;
+    }
+    
     //col 2
-        //if index 1 and 4 and 7 = compP:
-            //comp score = +10
-        //if index 1 and 4 and 7 = player:
-            //player score = -10
+    if(board[1] == player && board[4] == player && board[7] == player) {
+        score = 10;
+        return score;
+    }
     //col 3
-        //if index 2 and 5 and 8 = compP:
-            //comp score = +10
-        //if index 2 and 5 and 8 = player:
-            //player score = -10
-//3 in a row vert
-    //row 1
-        //if index 0 and 1 and 2 = compP:
-            //comp score = +10
-        //if index 0 and 1 and 2 = player:
-            //player score = -10
-    //row 2
-        //if index 3 and 4 and 5 = compP:
-            //comp score = +10
-        //if index 3 and 4 and 5 = player:
-            //player score = -10
-    //row 3
-        //if index 6 and 7 and 8 = compP:
-            //comp score = +10
-        //if index 6 and 7 and 8 = player]:
-            //player score = -10
+    if(board[2] == player && board[5] == player && board[8] == player) {
+        score = 10;
+        return score;
+    }
+//3 in a row horizontal:
+//row 1
+    if(board[0] == player && board[1] == player && board[2] == player) {
+        score = 10;
+        return score;
+    }
+//row 2
+    if(board[3] == player && board[4] == player && board[5] == player) {
+        score = 10;
+        return score;
+    }
+//row 3
+    if(board[6] == player && board[7] == player && board[8] == player) {
+        score = 10;
+        return score;
+    }
 //3 in a row diag
-    //if index 0 and 4 and 8 = compP
-        //comp score = +10
-    //if index 0 and 4 and 8 = player
-        //player score = -10
-    //if index 2 and 4 and 6 = compP
-        //comp score = +10
-    //if index 2 and 4 and 6 = player
-        //player score = -10
+    if(board[0] == player && board[4] == player && board[8] == player) {
+        score = 10;
+        return score;
+    }
+    if(board[2] == player && board[4] == player && board[6] == player) {
+        score = 10;
+        return score;
+    }   
+}
+
+
+
+//create compMove function:
+function compMove() {
+    //loop through the game board
+    for(i=0; i<currentBoard.length; i++){
+        //find first open spot:
+        if (currentBoard[i]!="X" && currentBoard[i]!="O") {
+            console.log(i);
+            //check if each open spot results in win:
+            //map currentboard to temp board:
+            tempBoard = currentBoard;
+            console.log(tempBoard);
+            //input player mark in open spot:
+            tempBoard[i] = "X";
+            //run test win function //Check problem here -----<<<<<
+            if (winningBoard(compP,tempBoard) == 10); {
+                return console.log(tempBoard);
+            }
+            //reset tempBoard to empty after each check;
+            tempBoard = [];
+            //if win update main board with comp mark and end loop
+
+        }
+        
+    }
+}
+    //if so pick spot and update board
+    //if not continue checking open spots for a win
+    //if a win is found pick spot and update board
+    //if no wins are found check all open spots for enemyplayer wins
+    //if win is found block that location
+    //if no enemy wins are found continue searching down another level
+
+
+
+//Call main game function:
+//winningBoard(compP, currentBoard);
+compMove();
 
 //Define win conditions
-//if score = 10;
-if(score == 10) {
-    //comp wins
-    console.log("comp wins!");
-}
-    
-//if player score = -10
-if(score == -10) {
-   //player wins
-    console.log("player wins!");
-}
-    
 //game ends and display if winner:
-
-// //Loop through the board and get each value:
-// for(i=0; i<currentBoard.length; i++) {
-//     console.log(currentBoard[i]);
+// if(winningBoard(compP, currentBoard) && score == 10) {
+//     //comp wins
+//     console.log("comp wins!")
+//     //displayMsg("comp wins!");
 // }
+    
+// //if player score = -10
+// if(winningBoard(player, currentBoard) && score == 10) {
+//    //player wins
+//     console.log("player wins!")
+//     //displayMsg("player wins!");
+// }
+// else if(score != 10) {
+//     console.log("neither wins");
+// }
+
+// console.log(score + ` score`);
+
 //create score board
 //create logic that chooses final store
 //create function that generates possible states
